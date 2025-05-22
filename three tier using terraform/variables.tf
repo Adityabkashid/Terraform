87167f030ba53ff74887d21a2f6166e7ec7b8bab
Aditya Kashid <adityakashid1612@gmail.com>
2025-05-22 19:34:05 +0530

    variables.tf

---
+++ three tier using terraform/variables.tf
@@ -0,0 +1,36 @@
+** variables.tf
+variable "my_region" {
+
+   type = string
+
+}
+
+variable "access_key" {
+
+   type = string
+
+}
+
+variable "secret_key" {
+
+   type =string
+
+}
+
+variable "my_ami" {
+
+   type = string
+
+}
+variable "vpc_id" {
+
+  type = string
+
+}
+
+variable "ins_type" {
+
+  type = string
+
+}
+ 
